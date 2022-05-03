1be30acda205b81f4bd9db55a65cc18b639dd372
EmilRR24 <grimlei2005@yahoo.com>
2022-05-03 06:58:21 -1000

    selection sort solution

--- algos/selectionSort.js
+++ algos/selectionSort.js
@@ -1,14 +1,22 @@
 /* 
-  https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
-  Stable sort
+  https://visualgo.net/en/sorting
+    
+  Selection sort works by iterating through the list, finding the maximum
+  value, and moving it to the end of the list. Then, ignoring the last
+  position, which is now sorted, iterate through the list again to find the
+  next maximum value and move it to the end. This continues until all values
+  are sorted.
+  Unstable sort.
   
   Time Complexity
-    - Best: O(n) linear when array is already sorted.
+    - Best: O(n^2) quadratic.
     - Average: O(n^2) quadratic.
-    - Worst: O(n^2) quadratic when the array is reverse sorted.
+    - Worst: O(n^2) quadratic.
   Space: O(1) constant.
-  For review, create a function that uses BubbleSort to sort an unsorted array in-place.
-  For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
+  Selection sort is one of the slower sorts.
+  - ideal for: pagination, where page 1 displays 10 records for example,
+      you run selection sort for 10 iterations only to display the first 10
+      sorted items.
 */
 
 const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -17,38 +25,33 @@ const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 /**
- * Sorts the given nums in-place by mutating the array.
- * Best: O(n) linear when array is already sorted.
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
  * Average: O(n^2) quadratic.
- * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * Worst: O(n^2) quadratic.
  * @param {Array<number>} nums
- * @returns {Array<number>} The given nums after being sorted.
+ * @returns {Array<number>} The given array after being sorted.
  */
-function bubbleSort(nums) {
+function selectionSort(nums) {
     // LOOP TO START THE SORTING FROM BEGINNING
-    var j = nums.length
-    let swap = false;
-    while(j > 1){
+    for(let i=0; i<nums.length; i++) {
+        let largest = 0;
         // LOOP ITERATE FROM BEGINNING OF THE ARRAY TO THE END
-        for(i=0; i<nums.length-1;i++) {
-            // IF THE LEFT VALUE IS BIGGER, THEN SWAP THE VALUES
-            if(nums[i] > nums[i+1] ){
-                swap = true
-                let tempVal = nums[i]
-                nums[i] = nums[i+1]
-                nums[i+1] = tempVal
-                
+        for(let j=1; j<nums.length-i; j++) {
+            // AS WE ITERATE THROUGH ARRAY, FIND THE INDEX OF THE LARGEST VALUE
+            if(nums[largest] < nums[j]){
+                largest = j;
             }
-            // COMPARE THE ADJACENT VALUES, IF THE RIGHT VALUE IS BIGGER NO SWAP
+            if (j === nums.length-i-1){
+                // [nums[largest], nums[j]] = [nums[j], nums[largest]]
+                temp = nums[largest];
+                nums[largest] = nums[j];
+                nums[j] = temp;
+            }
         }
-        if(swap === false){
-            return nums;
-        }
-        j--;
+        // SWAP THE LARGEST INDEX WITH THE END OF THE UNSORTED SECTION OF ARRAY
         console.log(nums)
     }
     return nums;
-    // OPTIMIZE: HAVE THE INNER FOR LOOP STOP 1 VALUE LESS ON EVERY ITERATION
-    // OPTIMIZE: CHECK TO SEE IF THERE WAS A SWAP IN INNER FOR LOOP
 }
-console.log(bubbleSort(numsOrdered))
+console.log(selectionSort(numsRandomOrder));
