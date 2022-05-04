d25748350423507df925d428e80af3c4c3fa7691
EmilRR24 <grimlei2005@yahoo.com>
2022-05-04 13:18:14 -1000

    added more functionalities to first_react

--- first_react/src/App.js
+++ first_react/src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 //IMPORTING ALL THE COMPONENTS WITH
 import Display from './components/Display'
 import Form from './components/Form'
+import {Component} from 'react'
 
-function App() {
-  return (
-    <fieldset>
-      <legend>App.js</legend>
-      <h1>React</h1>
-      <Display />
-      <Form/>
-    </fieldset>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicked: 0
+    }
+  }
+
+  clickHandler = () => {
+    let {clicked} = this.state
+    this.setState ({
+      'clicked': clicked+1
+    })
+  }
+
+  render() {
+    return (
+      <fieldset>
+        <legend>App.js</legend>
+        <h1>React</h1>
+        <button onClick={this.clickHandler}>Click Me!</button>
+        <Display firstName="Ray" lastName="R" age={35} clicked={this.state.clicked}/>
+        <Display firstName="Arya" lastName="V" age={2} clicked={this.state.clicked}/>
+        <Form/>
+      </fieldset>
+    );
+  }
 }
 
 export default App;
--- first_react/src/components/Display.jsx
+++ first_react/src/components/Display.jsx
@@ -4,10 +4,16 @@ import React, {Component} from "react"
 // CREATE THE COMPONENT 
 class Display extends Component {
     render() {
+        const {firstName, lastName, age, clicked} = this.props;
         return (
             <fieldset>
                 <legend>Display.jsx</legend>
                 <h1>Hello</h1>
+                <h1>First Name: {firstName}</h1>
+                <h1>Last Name: {lastName}</h1>
+                <h1>Age: {age}</h1>
+                <h1>Clicks: {clicked}</h1>
+                <h1></h1>
             </fieldset>
         )
     }
