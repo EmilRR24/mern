64b13a85df1acaeb37b782c1efcbeac7dcd055c6
EmilRR24 <grimlei2005@yahoo.com>
2022-05-05 11:11:16 -1000

    used destructuring

--- putting_it_together/src/components/Display.jsx
+++ putting_it_together/src/components/Display.jsx
@@ -4,12 +4,13 @@ import React, {Component} from "react"
 // CREATE THE COMPONENT 
 class Display extends Component {
     render() {
-        const {firstName, lastName, age, haircolor} = this.props;
+        // const {firstName, lastName, age, haircolor} = this.props;
+        
         return (
             <div>
-                <h1>{lastName}, {firstName}</h1>
-                <h3>Age: {age}</h3>
-                <h3>hair Color: {haircolor}</h3>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <h3>Age: {this.props.age}</h3>
+                <h3>hair Color: {this.props.haircolor}</h3>
             </div>
         )
     }
