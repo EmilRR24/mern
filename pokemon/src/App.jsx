40241f6bb578182ab6ea523af034aecf24d12391
EmilRR24 <grimlei2005@yahoo.com>
2022-05-10 08:18:26 -1000

    completed get function and response for axios

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,30 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const Example = (props) => {
-  const [people, setPeople] = useState([]);
+  // ----- VANILLA STATE ---------------//
+  // const [pokemon, setPokemon] = useState([]);
+
+  const [pokemon, setPokemon] = useState();
 
   const pokemonHandler = () => {
-      fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
-        //SUCCESSFUL RESPONSE
-        .then(response => response.json())
-        .then(response => setPeople(response.results))
-        // UNSUCCESSFUL RESPONSE
-        .catch(err => console.error("ERROR", err))
+    // --------------VANILLA JS METHOD (FETCH)-----------//
+    // fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      // SUCCESSFUL RESPONSE
+      // .then(response => response.json())
+      // .then(response => setPokemon(response.results))
+      // UNSUCCESSFUL RESPONSE
+      // .catch(err => console.error("ERROR", err))
+
+    // -------------AXIOS----------------------------//
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      // SUCCESSFUL RESPONSE
+      .then(response => {
+        console.log("SUCCESS", response.data) 
+        setPokemon(response.data)
+      })
+      // UNSUCCESSFUL RESPONSE
+      .catch(err => console.error("ERROR", err)) 
   };
 
-  
+
 
   return (
     <fieldset style={{textAlign:'center'}}>
       <div>
         {/* <legend>App.js</legend> */}
         <button onClick={pokemonHandler}>Fetch Pokemon</button>
-        {people.length > 0 && people.map((person, index)=>{
-          return (<li key={index}>{index}: {person.name}</li>)
-        })}
+        {
+        // ----- VANILLA FUNCTION TO DISPLAY DATA ----------------------//
+        /* {pokemon.length > 0 && pokemon.map((poke, index)=>{
+          return (<li key={index}>{index}: {poke.name}</li>)
+        })} */
+
+        pokemon &&
+        pokemon.length > 0 && pokemon.map((poke, index)=>{
+          return (<li key={index}>{poke.name}</li>)})
+          
+        }
       </div>
     </fieldset>
   );
