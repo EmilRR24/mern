6261301141fc658f8f7a7e1ecd9564106bbdb262
EmilRR24 <grimlei2005@yahoo.com>
2022-05-10 10:54:17 -1000

    initial commit for routing practice

---
+++ routing/src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const Num = (props) => {
+    // return (
+
+    // )
+
+  }
+  return (
+    <BrowserRouter>
+    <h1>Rooting Example</h1>
+    <p>
+
+    </p>
+    </BrowserRouter>
+  );
+}
+
+export default App;
