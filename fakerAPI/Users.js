97dbddf2018afa1b8792be847e04acec259e240f
EmilRR24 <grimlei2005@yahoo.com>
2022-05-12 11:45:08 -1000

    added id to user class

--- fakerAPI/Users.js
+++ fakerAPI/Users.js
@@ -7,6 +7,7 @@ class Users {
         this.phoneNumber = faker.phone.phoneNumber()
         this.firstName = faker.name.firstName()
         this.lastName = faker.name.lastName()
+        this.id = faker.datatype.number()
     }
 }
 
