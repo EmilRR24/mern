e0d72601c3bf040d623a4a66a3af6206907c17b4
EmilRR24 <grimlei2005@yahoo.com>
2022-05-23 12:08:07 -1000

    moved mern group project to seperate repo

--- group_mern/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1/gamers",{
-    useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-.then(()=> console.log("CONNECTED TO GAMERS"))
-.catch(err => console.log("ERROR: ", err))
