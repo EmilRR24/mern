ee595509e038cd79a47b3bcc151e6d1da16d0f38
EmilRR24 <grimlei2005@yahoo.com>
2022-05-23 11:29:10 -1000

    initial commit for group mern project

--- algos/flattenObjectOfArrays.js
+++ algos/flattenObjectOfArrays.js
@@ -2,27 +2,27 @@ const object1 = {
   closedCreditMemos: [],
   closedDeliveryOrders: [],
   closedPickupOrders: [
-      { id: 112, type: "pickup" },
-      { id: 117, type: "pickup" },
+    { id: 112, type: "pickup" },
+    { id: 117, type: "pickup" },
   ],
   openCreditMemos: [],
   openDeliveryOrders: [
-      {
-          id: 123,
-          type: "delivery",
-          gateCode: "#2552",
-      },
-      {
-          id: 153,
-          type: "delivery",
-          instructions: "Place in secure delivery box.",
-      },
+    {
+      id: 123,
+      type: "delivery",
+      gateCode: "#2552",
+    },
+    {
+      id: 153,
+      type: "delivery",
+      instructions: "Place in secure delivery box.",
+    },
   ],
   openPickupOrders: [
-      {
-          id: 123,
-          type: "pickup",
-      },
+    {
+      id: 123,
+      type: "pickup",
+    },
   ],
   returnPickupOrders: [],
 };
@@ -36,24 +36,24 @@ const expected1 = [
 ];
 
 /**
-* Takes an object containing arrays of objects and places all the nested
-* objects into a new one-dim array.
-* - Time: O(?).
-* - Space: O(?).
-* @param {Object} o Containing arrays of objects.
-* @returns {Object[]} An array of objects.
-*/
+ * Takes an object containing arrays of objects and places all the nested
+ * objects into a new one-dim array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object} o Containing arrays of objects.
+ * @returns {Object[]} An array of objects.
+ */
 function flattenObjectOfArrays(obj) {
   const retArr = [];
   for (let v of Object.values(obj)) {
-      console.log(v)
-      if (v) {
-          v.forEach(element => {
-              let {id, type} = element
-              retArr.push( {id, type})
-          });
-      }
+    console.log(v);
+    if (v) {
+      v.forEach((element) => {
+        let { id, type } = element;
+        retArr.push({ id, type });
+      });
+    }
   }
-  return retArr
+  return retArr;
 }
-console.log(flattenObjectOfArrays(object1))
+console.log(flattenObjectOfArrays(object1));
